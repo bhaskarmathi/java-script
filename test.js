3f9093367252493d23d3ab2d8b27244dd69a99d0
bhaskar mathi <BHaskarmathi1411@gmail.com>
2025-12-22 14:33:39 +0530

    Initial commit with HTML and JavaScript files

---
+++ test.js
@@ -0,0 +1,168 @@
+// var :- we can re-assign the valuable
+        //   update value
+        // function scope
+
+
+
+// var number = 100;
+
+// console.log(number);//magic key of outout
+
+      
+
+
+// let
+     // we can`t re-assign the  value
+     //  can`t update the value
+    //  function in blocked{}
+
+
+
+//   {
+//     let a=10;
+//     let b=20;
+//     console.log(a+b);
+// } 
+
+
+
+
+// const 
+    // can`t be re-assign the vale
+    // can`t be updated
+    // function in blocked scope{}
+    // declare the value before the initialize 
+    
+    
+// {
+//     const a=20;
+//     const b=10;
+//     console.log(a,b);
+// }
+
+
+
+
+
+// primitive data types(number,string,boolean,bigint,symbol,null,undefined)
+
+
+
+  //  number define a numeric for variable
+// var x=50;
+// console.log(typeof(x));
+// console.log(x);
+
+
+
+  //  string  values which is stored in "";
+//  var a ="pavan";
+// var b = "lizard";
+
+// console.log(typeof(a,b));
+// console.log(a,b);
+
+
+
+// boolean  values can be one of two values: true or false, representing the truth value of a logical proposition.
+    
+//  let isLoggedIn =true;
+    // console.log(typeof(isLoggedIn));
+
+
+
+
+//   bigint BigInt values represent integer values which are too high or too low to be represented by the number 
+
+// const alsoHuge = BigInt(9007199254740991);
+//  console.log(typeof(alsoHuge))
+
+
+
+
+// undefined  giving an empty value for the variable
+
+// var a =
+// console.log(typeof(a));
+
+
+
+// symbol Symbol is a built-in object whose constructor returns a symbol primitive
+//  — also called a Symbol value or just a Symbol
+// const id= Symbol();
+// console.log(typeof(id));
+
+
+
+
+// null giving null value for the variable
+// const a = null
+// console.log((a));
+
+
+
+
+
+  //  arithmetic operations
+// post increment a++
+// pre increment ++a
+
+
+
+// mathematical operators
+// add,sub,multiplication,division,
+
+
+// let a = 10;
+//   let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// var a = 10;
+
+//  console.log(a);
+
+//  let a = 20;
+//   let b = 10;
+//   console.log(a+b);
+
+
+//   const c = 50;
+
+
+
+let a = 10;
+console.log(typeof(a));
+
+var b = "30";
+console.log(typeof(b));
+
+const c = (true);
+console.log(typeof(c));
+
+const userId = (symbol());
+console.log(typeof(userId));
+
+const highScore = (bigInt (142585798541855));
+console.log(typeof(highscore));
+
+
+let h = null;
+console.log((h));
+
+let j = 
+console.log(typeof(j)); 
+
+
+
+ 
+
+
+
+
+
+
+
