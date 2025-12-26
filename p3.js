c9ef361c3de7482b63de2b3133a5d96810a9478d
bhaskar Mathi <BHaskarmathi1411@gmail.com>
2025-12-26 04:01:34 +0000

    Updated p3.js and added p4.js

--- p3.js
+++ p3.js
@@ -162,20 +162,20 @@
 //    sum = sum + i + "*"
 // }console.log(sum);
 
-let sum = 0;
-for (let i = 1;i<20;i++){
-  if ((i % 2==0)&&(i % 5==0)){
-   sum = sum +i
-   }
+// let sum = 0;
+// for (let i = 1;i<20;i++){
+//   if ((i % 2==0)&&(i % 5==0)){
+//    sum = sum +i
+//    }
 
-}
-console.log(sum);
+// }
+// console.log(sum);
 
 
   
 
 // let array = [1,2,3,4,5,];
-// for(let i = 1;i<=5;i++){
+// for(let i = 1;i = array.length;i++){
 //    if(i % 2===0){
 //       console.log(i)
 //    }
@@ -206,11 +206,10 @@ console.log(sum);
 //       output = output +"tond";
 //    }else{
 //       output = output + a[i]
+//     }
+//  }console.log(output);
 
-//    }
-// }console.log(output);
-
-// // toString
+//  toString
 // let numbers = ["1","2","3","4","5"];
 // let my_list = numbers.toString();
 // console.log(my_list);
@@ -242,6 +241,88 @@ console.log(sum);
 
 
 
+// let age = 15;
+//   let text = "you can drive"
+// if(age =20){
+//   text = "you can`t drive"
+// }console.log(text);
+
+    
+
+
+// for (let day = 1; day<=4; day ++){
+//   console.log("day",day);
+//     for (let sub = 1;sub <=4;sub++){
+//       console.log("sub",sub)
+       
+//     }; 
+//   };
+
+
+// for (let hour= 1; hour <= 4;hour++){
+  
+//   for(let secs =1;secs<=6; secs++){
+    
+//      for(let min=1; min<=6; min++){
+       
+//        console.log("secs",secs); 
+//     };
+    
+//   console.log( "min completed");
+//   };
+
+//   console.log  ( "hour completd");
+// };
+
+
+
+// for(let farm=1;farm<=5;farm++){
+//   console.log("farm",farm);
+//   for(let seed =1;seed<=5;seed++){
+//     console.log("*");
+//   };
+// };
+
+
+// for(let farm = 1;farm <=5;farm++){
+//   console.log("farm",farm)
+//   let bag = "";
+//     for(let seed=1;seed<=5;seed++){
+//     bag = bag + "*" +  " "
+//     }
+// console.log(bag);
+// };
+
+
+
+
+
+
+// for(let i = 1; i <=5; i++){
+//   let bag = "";
+//   for(let j=1; j<=i; j++){
+//     bag=bag + "*";
+    
+//   };
+//   console.log(bag);
+// };
+
+
+// for(let hor = 1;hor<=10;hor++){ 
+//   let bag = "";
+//     for(let ver =1;ver<=10;ver++){ 
+//        if(hor==1 || hor==10){ 
+//           bag = bag + "*";
+//        }
+//          else if(ver==1 || ver==10){
+//            bag = bag + "*";     
+//         } else{
+//              bag = bag + " ";
+//          }     
+       
+//      }
+//           console.log(bag); 
+// // };``
 
 
 
@@ -252,11 +333,89 @@ console.log(sum);
 
 
 
+// let a = [1,2,3,4,5,6,7,8,9,10];
+// for(let i=1;i<=a.length;i++){
+//   if(i % 2 ===0){
+//     console.log(i);
+//   }
+  
+// };
+
+// let a = "pavan";
+// let output = "";
+// for(i=0;i<a.length;i++){
+//   if(a[i] === "p"){
+//   output = output + "tonda"
+    
+//   }else{
+//     output = output  + a[i]
+//     }
+     
+// }
+// //  console.log(output);
+
+// let year = 2024;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//   console.log("Leap Year");
+// } else {
+//   console.log("Not a Leap Year");
+// }
+
+
+
+//Write a program to check if a number is positive or negative
 
 
 
 
+// let num = -5
+// if(num>=0){
+//   consple.log("positive no");
+// }else{
+//   consol.log("negitive no")
+// };
 
 
 
+//   let num = 123456;
+// let count = 0;
+// for(;num > 0;num = Math.floor(num/10)){
+//   count++;
+// }console.log(count);
 
+
+
+// let num = 9874575457555454545544545786123;
+// let count = 0;
+// for(; num>0;num=Math.floor(num/10)){
+//   count ++;
+// }
+// console.log(count);
+
+// let num =5;
+// let fact = 1;
+// for(let i =1;i<=5;i++){
+//   fact*=i
+// }console.log(fact);
+
+
+// let name = "pavan";
+// let array ="";
+// for(let i = 0;i<name.length;i++){
+//   if(name[i]==="p"){
+//     array=array+"thonda"
+//   }else{
+//     array = array+name[i];
+//   }
+  
+// }
+// console.log(array);
+
+
+// let num = 5;
+// let fact =1;
+// for(let num=1;num<=5;num++){
+//   fact *=num
+// }
+// console.log(fact);
